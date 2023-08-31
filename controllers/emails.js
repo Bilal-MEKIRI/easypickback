34a65bec54a561cf7ebb5637b499e0c1c3347074
Bilal-MEKIRI <bilal.mekiri@gmail.com>
2023-08-31 23:37:41 +0200

    sendGrid instead of nodemailer

--- controllers/emails.js
+++ controllers/emails.js
@@ -1,6 +1,9 @@
 const Emails = require("../models/emails.js");
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
 const { validationResult } = require("express-validator");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const getEmails = async (req, res) => {
   try {
@@ -29,22 +32,11 @@ const postEmails = async (req, res) => {
     });
     const savedEmail = await newEmail.save();
 
-    // Create a transporter using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
-
-    // Send an email notification
-    const mailOptions = {
-      from: process.env.EMAIL, // Replace with the recipient's email
-      to: process.env.EMAIL, // Replace with your email
+    const msg = {
+      to: process.env.EMAIL,
+      from: process.env.EMAIL,
       subject: "New Email From EasyPick",
+      text: `You've received a new email from ${firstName} ${lastName}.\nSender email: ${email}.\n\nMessage:\n${message}`,
       html: `
     <p>You've received a new email from ${firstName} ${lastName}.</p>
     <p>Sender email: ${email}.</p>
@@ -53,15 +45,48 @@ const postEmails = async (req, res) => {
   `,
     };
 
-    await transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error("Error sending email:", error);
-      } else {
-        console.log("Email sent:", info.response);
-      }
-    });
+    try {
+      await sgMail.send(msg);
+      console.log("Email sent");
+      res.status(200).json(savedEmail);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: error.message });
+    }
 
-    res.status(200).json(savedEmail);
+    // Create a transporter using the default SMTP transport
+    // const transporter = nodemailer.createTransport({
+    //   host: "smtp.gmail.com",
+    //   port: 587,
+    //   secure: false,
+    //   auth: {
+    //     user: process.env.EMAIL,
+    //     pass: process.env.EMAIL_PASSWORD,
+    //   },
+    // });
+
+    // Send an email notification
+    //   const mailOptions = {
+    //     from: process.env.EMAIL, // Replace with the recipient's email
+    //     to: process.env.EMAIL, // Replace with your email
+    //     subject: "New Email From EasyPick",
+    //     html: `
+    //   <p>You've received a new email from ${firstName} ${lastName}.</p>
+    //   <p>Sender email: ${email}.</p>
+    //   <p><strong>Message:</strong></p>
+    //   <p>${message}</p>
+    // `,
+    //   };
+
+    //   await transporter.sendMail(mailOptions, (error, info) => {
+    //     if (error) {
+    //       console.error("Error sending email:", error);
+    //     } else {
+    //       console.log("Email sent:", info.response);
+    //     }
+    //   });
+
+    // res.status(200).json(savedEmail);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: error.message });
