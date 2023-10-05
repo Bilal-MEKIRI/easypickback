1c18847307292e7543614841578ffb49cd18d473
Bilal-MEKIRI <bilal.mekiri@gmail.com>
2023-10-05 19:18:13 +0200

    modified the mongoose model for users by removing the need of a userName

--- src/models/users.js
+++ src/models/users.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const usersSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
   loginAttempts: { type: Number, default: 0 }, // Tracks the number of failed login attempts
   lockUntil: { type: Date, default: null }, // Timestamp indicating when the account will be unlocked
 });
